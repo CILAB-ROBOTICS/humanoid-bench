b49ea0c204ba57b0f622e1e192c7d38f32e7825c
In-Chang Baek <bic4907@gmail.com>
2025-05-10 14:27:35 +0900

    fix run.sh to works on /bin/bash

--- run.sh
+++ run.sh
@@ -7,6 +7,10 @@ CMD=${@:-/bin/bash}
 USER_NAME=$(whoami)
 GPU_ID=$CUDA_VISIBLE_DEVICES
 CMD_SUMMARY=$(echo "$CMD" | tr -d ' ' | cut -c1-10)
+# replace / in CMD_SUMMARY with _
+CMD_SUMMARY=$(echo "$CMD_SUMMARY" | tr '/' '_')
+
+
 TIME_TAG=$(date +%H%M%S)
 CONTAINER_NAME="${USER_NAME}_gpu${GPU_ID}_${CMD_SUMMARY}_${TIME_TAG}"
 
