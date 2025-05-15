e46b025fce0d4302eeab7a27579d441ad2b8aa35
In-Chang Baek <bic4907@gmail.com>
2025-05-15 14:26:49 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ GPU_ID=$CUDA_VISIBLE_DEVICES
 CMD_SUMMARY=$(echo "$CMD" | tr -d ' ' | cut -c1-10)
 # replace / in CMD_SUMMARY with _
 CMD_SUMMARY=$(echo "$CMD_SUMMARY" | tr '/' '_')
-
+CMD_SUMMARY=$(echo "$CMD_SUMMARY" | tr '=' '-')
 
 TIME_TAG=$(date +%H%M%S)
 CONTAINER_NAME="${USER_NAME}_gpu${GPU_ID}_${CMD_SUMMARY}_${TIME_TAG}"
